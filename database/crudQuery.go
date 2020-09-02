afeeef82f536ae745e1a76e8849a3bf8e729a3f9
dnovaes <novaesdigo@gmail.com>
2020-09-02 01:48:59 -0300

    Add insert, delete, findall queries

---
+++ database/crudQuery.go
@@ -0,0 +1,51 @@
+package database
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func getCollection(collName string) *mongo.Collection {
+	var client = Db.client
+	return client.Database(database).Collection(collName)
+}
+
+func insert(coll *mongo.Collection, doc primitive.D) *mongo.InsertOneResult {
+	result, err := coll.InsertOne(context.Background(), doc)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func delete(coll *mongo.Collection, doc primitive.D) *mongo.DeleteResult {
+	result, err := coll.DeleteOne(context.Background(), doc)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func deleteMany(coll *mongo.Collection, doc primitive.D) *mongo.DeleteResult {
+	result, err := coll.DeleteMany(context.Background(), doc)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func findAll(coll *mongo.Collection) []bson.M {
+	var results []bson.M
+	cursorResult, err := coll.Find(context.Background(), bson.D{})
+	if err != nil {
+		panic(err)
+	}
+
+	err = cursorResult.All(context.TODO(), &results)
+	if err != nil {
+		panic(err)
+	}
+	return results
+}
---
+++ database/crudQuery_test.go
@@ -0,0 +1,86 @@
+package database
+
+import (
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"testing"
+)
+
+func TestGetCollection(t *testing.T) {
+	StartConnection()
+
+	collection := getCollection("contacts")
+	assert.NotNil(t, collection)
+
+	StopConnection()
+}
+
+func TestInsert(t *testing.T) {
+	StartConnection()
+	coll := getCollection("contacts")
+
+	bsonDoc := bson.D{
+		{Key: "name", Value: "contactName #1"},
+		{Key: "email", Value: "askdjasdl@mail.com"},
+		{Key: "message", Value: "its a message #1"},
+		{Key: "createdAt", Value: 123455677},
+	}
+
+	result := insert(coll, bsonDoc)
+	assert.NotNil(t, result.InsertedID)
+	deleteMany(coll, bsonDoc)
+
+	StopConnection()
+}
+
+func TestFindAll(t *testing.T) {
+	StartConnection()
+
+	coll := getCollection("test_contacts")
+	coll.Drop(nil)
+
+	bsonDoc1 := bson.D{
+		{Key: "name", Value: "contactName #1"},
+		{Key: "email", Value: "askdjasdl@mail.com"},
+		{Key: "message", Value: "its a message #1"},
+		{Key: "createdAt", Value: 123455677},
+	}
+	insert(coll, bsonDoc1)
+	bsonDoc2 := bson.D{
+		{Key: "name", Value: "contactName #2"},
+		{Key: "email", Value: "askdjasdl@mail.com"},
+		{Key: "message", Value: "its a message #2"},
+		{Key: "createdAt", Value: 123455677},
+	}
+	insert(coll, bsonDoc2)
+	result := findAll(coll)
+	assert.Equal(t, 2, len(result))
+
+	coll.Drop(nil)
+	result = findAll(coll)
+	assert.Equal(t, 0, len(result))
+
+	StopConnection()
+}
+
+func TestDeleteOne(t *testing.T) {
+	StartConnection()
+
+	coll := getCollection("test_contacts")
+	coll.Drop(nil)
+
+	bsonDoc1 := bson.D{
+		{Key: "name", Value: "contactName #1"},
+		{Key: "email", Value: "askdjasdl@mail.com"},
+		{Key: "message", Value: "its a message #1"},
+		{Key: "createdAt", Value: 123455677},
+	}
+	insert(coll, bsonDoc1)
+	resultDelete := delete(coll, bson.D{
+		{Key: "name", Value: "contactName #1"},
+	})
+	assert.Equal(t, int64(1), resultDelete.DeletedCount)
+	coll.Drop(nil)
+
+	StopConnection()
+}
